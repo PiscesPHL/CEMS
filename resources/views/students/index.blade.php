e1e154d8cbdb2a9d9a1647ebf89ea2cb9925a57c
Evhelz <mf386300@gmail.com>
2026-06-30 15:25:58 +0800

    NEW 12

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -21,45 +21,41 @@
 
         <div class="card mb-4">
             <div class="card-body p-0">
-                <table class="table">
+                <table class="table table-bordered table-striped">
                     <thead>
                         <tr>
-                            <th style="width: 10px">#</th>
-                            <th>Student Number</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Email Address</th>
+                            <th>Student No.</th>
+                            <th>Email</th>
+                            <th>Name</th>
                             <th>Course</th>
                             <th>Year Level</th>
                             
                             @can('manage-students')
-                                <th>Action</th>
+                                <th>Actions</th>
                             @endcan
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach( $students as $key => $student )
+                        @foreach( $students as $student )
                         <tr class="align-middle">
-                            <td>{{ $key + 1 }}</td>
                             <td>{{ $student->student_number }}</td>
-                            <td>{{ $student->first_name }}</td>
-                            <td>{{ $student->last_name }}</td>
                             <td>{{ $student->email }}</td>
+                            <td>{{ $student->first_name }} {{ $student->last_name }}</td>
                             <td>{{ $student->course }}</td>
                             <td>{{ $student->year_level }}</td>
                             
                             @can('manage-students')
                             <td class="d-flex gap-2">
-                                <form action="{{ route('students.send-email', $student->id) }}" method="POST">
+                                @if(isset($student->email))
+                                <form action="{{ route('students.send-email', $student) }}" method="POST" class="d-inline" onsubmit="return confirm('Send welcome email to this student?')">
                                     @csrf   
-                                    <button type="submit" class="btn btn-info btn-sm text-white" onclick="return confirm('Send welcome email to {{ $student->first_name }}?')">
-                                        Email
-                                    </button>
+                                    <button type="submit" class="btn btn-sm btn-info text-white">Send Email</button>
                                 </form>
+                                @endif
 
-                                <a href="{{ url('students', $student->id) }}/edit" class="btn btn-success btn-sm">Edit</a>
+                                <a href="{{ route('students.edit', $student) }}" class="btn btn-sm btn-warning text-white">Edit</a>
                                 
-                                <form action="{{ url('students', $student->id) }}" method="POST">
+                                <form action="{{ url('students', $student->id) }}" method="POST" class="d-inline">
                                     @csrf   
                                     @method('DELETE')
                                     <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you want to delete this student?')">Delete</button>
