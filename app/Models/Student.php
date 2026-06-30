20070b46cbc00cc3a25f633238c45c6f79ba14a2
Evhelz <mf386300@gmail.com>
2026-06-30 13:31:31 +0800

    7

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -4,12 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes; // <-- 1. Import this
 
 class Student extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes; // <-- 2. Add it here next to HasFactory
 
-    // Adding the fillable array from your bootcamp slide
     protected $fillable = [
         'student_number',
         'first_name',
